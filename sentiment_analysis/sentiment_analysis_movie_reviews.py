451fcf111c91be546b9afb57b4713afdaca9af89
AldworthJohn <jbax.dev@gmail.com>
2018-06-19 16:05:40 +0800

    cleaning doc & defining a vocabulary

--- sentiment_analysis/sentiment_analysis_movie_reviews.py
+++ sentiment_analysis/sentiment_analysis_movie_reviews.py
@@ -5,7 +5,11 @@ Created on Tue Jun 19 12:22:22 2018
 @author: I800226
 """
 
+import string
+import re
 from os import listdir
+from nltk.corpus import stopwords
+from collections import Counter
 
 # load doc into memory
 def load_doc(filename):
@@ -16,20 +20,51 @@ def load_doc(filename):
     # close the file
     file.close()
     return text
+        
+# turn a doc into clean tokens
+def clean_doc(doc):
+    # split into tokens using white space
+    tokens = doc.split()
+    # prepare regex for char filtering
+    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
+    # remove punctuation from each word
+    tokens = [re_punc.sub('', w) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    # filter out short tokens
+    tokens = [word for word in tokens if len(word) > 1]
+    return tokens
+
+# load doc and add to vocab
+def add_doc_to_vocab(filename, vocab):
+    # load doc
+    doc = load_doc(filename)
+    # clean doc
+    tokens = clean_doc(doc)
+    # update counts
+    vocab.update(tokens)
 
 # load all docs in a directory
-def process_docs(directory):
-    #walk through all files in the folder
+def process_docs(directory, vocab):
+    # walk through all files in the folder
     for filename in listdir(directory):
-        # skip files that do not have the right extension
-        if not filename.endswith(".txt"):
-            next
+        # skip any reviews in the test set
+        if filename.startswith("cv9"):
+            continue
         # create the full path of the file to open
         path = directory + '/' + filename
-        # load document
-        doc = load_doc(path)
-        print('Loaded %s' % filename)
-        
-# specify directory to load
-directory = 'txt_sentoken/neg'
-process_docs(directory)
+        # add doc to vocab
+        add_doc_to_vocab(path,vocab)
+
+# define vocab
+vocab = Counter()
+#add all docs to the vocab
+process_docs('txt_sentoken/pos', vocab)
+process_docs('txt_sentoken/neg', vocab)
+# print the size of the vocab
+print(len(vocab))
+# print the top words in the vocab
+print(vocab.most_common(50))
